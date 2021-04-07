f1ef330f6e5e4b5a49942bd8257541afc2f72604
Toni Powell <tonipow3ll@gmail.com>
2021-04-07 14:47:05 -0400

    getting somewhere

--- routes/auth.js
+++ routes/auth.js
@@ -14,4 +14,11 @@ router.get('/google/callback', passport.authenticate('google', {failureRedirect:
     res.redirect('/dashboard')
 })
 
+// logout user
+// auth/logout
+
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
 module.exports = router
