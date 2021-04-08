ac9fb2146480de3133216698fe0eb7d19f3acd7f
Toni Powell <tonipow3ll@gmail.com>
2021-04-08 10:58:31 -0400

    port update

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
 app.use('/posts', require('./routes/posts'))
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT 
+// || 5000;
 
 app.listen(PORT, console.log(`Server listening on PORT ${PORT}!`))
