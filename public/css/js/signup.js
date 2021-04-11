5b9df4ee1e738eca50b43eb067fa8fde27219c56
Toni Powell <tonipow3ll@gmail.com>
2021-04-11 19:37:49 -0400

    workin on it

--- public/css/js/signup.js
+++ public/css/js/signup.js
@@ -1,26 +1,26 @@
-$(document).ready(function () {
-    console.log("ready!");
+// $(document).ready(function () {
+//     console.log("ready!");
 
-const loginFormHandler = (event) => {
+// const loginFormHandler = (event) => {
 
-    //NEED TO CHANGE DOCUMENT.LOCATION.REPLACE WITH /USER INFO HERE
-    event.preventDefault();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-    if (email && password) {
-      const response = fetch('/signup', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      }).then((response) => {
-        if (response.ok) {
-          console.log(response)
-          document.location.replace('/');
-        } else {
-          alert('Failed to sign up.');
-        }
-      });
-    }
-  };
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-})
+//     //NEED TO CHANGE DOCUMENT.LOCATION.REPLACE WITH /USER INFO HERE
+//     event.preventDefault();
+//     const email = document.querySelector('#email-signup').value.trim();
+//     const password = document.querySelector('#password-signup').value.trim();
+//     if (email && password) {
+//       const response = fetch('/signup', {
+//         method: 'POST',
+//         body: JSON.stringify({ email, password }),
+//         headers: { 'Content-Type': 'application/json' },
+//       }).then((response) => {
+//         if (response.ok) {
+//           console.log(response)
+//           document.location.replace('/');
+//         } else {
+//           alert('Failed to sign up.');
+//         }
+//       });
+//     }
+//   };
+//   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+// })
